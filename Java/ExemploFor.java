3afc875f641decc3a6cc6aa36d47e001bac52850
Maria Isabel <129692537+mariaisabelcoms@users.noreply.github.com>
2024-02-07 17:26:09 +0000

    ExemploFor Aula 5

---
+++ Java/ExemploFor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ExemploFor {
+    public static void main(String[]args) {
+
+        double nota1, nota2, media;
+        Scanner input = new Scanner(System.in);
+
+        for(int na=1;na<=5;na++) {
+
+            System.out.println("Aluno" + na);
+            System.out.println("Digite a nota 1: ");
+            nota1 = input.nextDouble();
+    
+            System.out.println("Digite a nota 2: ");
+            nota2 = input.nextDouble();
+    
+            media = (nota1+nota2)/2;
+            System.out.println("Média: " + media);
+
+        }
+
+
+
+
+    }
+}
